103f200d842c62797ea8fa0eb9a590d960a7040b
Jeremy Valance <valancejeremy@gmail.com>
2015-12-07 22:13:07 -0500

    update config

---
+++ pdoConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = 'webdb.uvm.edu';
+$username_admin = 'jvalance_admin';
+$password_admin = 'lbSBxU3OVn98';
+$dbname = 'JVALANCE_final';
+
+$username_writer = "jvalance_writer";
+$password_writer = "yRyhY7N9aTvgK5JY";
+$username_reader = "jvalance_reader";
+$password_reader = "8a7Q861fLNN4";
+
+
+?>
